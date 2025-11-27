24c679f310db129b7666e55541fab34acb4a0d57
Minakshi Kumari <jhaminakshi333@gmail.com>
2025-11-27 16:21:09 +0545

    Adding cors and modifying client & server side accordingly to fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect (() => {
+    fetchNotes();
+  },[])
+
+  const fetchNotes = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/notes/");
+      const data = await response.json()
+      setNotes(data);
+      console.log(data)
+    } catch (err) {
+      console.log(err);
+      
+    }
+  }
 
   return (
     <>
       <h1>Personal Notes</h1>
-      <label htmlFor="name">Name: </label>
-      <input type="text" placeholder='Enter Your Name' /> <br /> 
-      <label htmlFor="desc">Note Description : </label>
-      <input type="text" placeholder='Write Your Notes Here' /> <br />
+      <div>
+            <label htmlFor="name">Name: </label>
+            <input type="text" placeholder='Enter Your Name' /> <br /> 
+            <label htmlFor="desc">Note Description : </label>
+            <input type="text" placeholder='Write Your Notes Here' /> <br />
+            <button>Add Note</button>
+      </div>
 
-      <button>Add Note</button>
+      <div>
+        <h1>Display Notes</h1>
+        {notes.map((item)=>(
+          <div key={item.id}>
+            <p>Name : {item.userName} </p>
+            <p>Descriptions : {item.desc}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
--- server/api/urls.py
+++ server/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_notes , create_notes
 
 urlpatterns = [
-    path('notes', get_notes, name='get_notes'),
+    path('notes/', get_notes, name='get_notes'),
     path('notes/create/' , create_notes , name='create_notes')
 ]
